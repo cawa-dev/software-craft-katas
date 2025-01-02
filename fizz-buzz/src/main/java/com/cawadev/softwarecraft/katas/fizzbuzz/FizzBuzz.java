bbf94e40b9cd9523aac8a616beeceb3fe58aa7da
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-02 18:47:10 +0100

    feat: green step for the second rule 'should return Fizz when number is multiple of 3'

--- fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,10 @@ package com.cawadev.softwarecraft.katas.fizzbuzz;
 public record FizzBuzz() {
 
     public String print(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 }
