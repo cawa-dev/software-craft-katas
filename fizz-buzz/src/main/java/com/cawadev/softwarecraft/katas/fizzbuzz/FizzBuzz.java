2ae45c313e8475f6d1c688c9cb7b1f101316d11e
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-02 18:49:01 +0100

    feat: refacto step for the second rule 'should return Fizz when number is multiple of 3'

--- fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,10 @@ package com.cawadev.softwarecraft.katas.fizzbuzz;
 
 public record FizzBuzz() {
 
+    private static final int FIZZ_MULTIPLIER = 3;
+
     public String print(int number) {
-        if (number % 3 == 0) {
+        if (number % FIZZ_MULTIPLIER == 0) {
             return "Fizz";
         }
 
