96597dc11df44b1f10961760d91ae88f2edef6af
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-02 18:59:02 +0100

    feat: green step for the fourth rule 'should return FizzBuzz when number is multiple of 3 and 5'

--- fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/cawadev/softwarecraft/katas/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,10 @@ public record FizzBuzz() {
     private static final int FIZZ_MULTIPLIER = 3;
 
     public String print(int number) {
+        if (number % FIZZ_MULTIPLIER == 0 && number % BUZZ_MULTIPLIER == 0) {
+            return "FizzBuzz";
+        }
+
         if (number % BUZZ_MULTIPLIER == 0) {
             return "Buzz";
         }
