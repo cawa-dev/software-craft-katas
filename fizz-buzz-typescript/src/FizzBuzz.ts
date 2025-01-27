987c8ec9e000c0b22d87eadd332ad3c145904917
Sacha MOUCHON <sacha.mouchon.carbon-it@ext.pmu.fr>
2025-01-27 17:56:49 +0100

    feat: add the case for multiple of 5 and 3  and should return Buzz and Fizz in typescript kata

--- fizz-buzz-typescript/src/FizzBuzz.ts
+++ fizz-buzz-typescript/src/FizzBuzz.ts
@@ -2,6 +2,14 @@ export const fizzBuzz: {
     evaluate: (number: number) => string
 } = ({
     evaluate: (number: number): string => {
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return number.toString();
     }
 });
--- fizz-buzz-typescript/tests/FizzBuzz.test.ts
+++ fizz-buzz-typescript/tests/FizzBuzz.test.ts
@@ -12,4 +12,27 @@ describe(('FizzBuzz'), () => {
         // Assert
         expect(value).toBe("1");
     });
+
+
+    it("Should return Fizz when number is multiple of three", () => {
+        // Arrange
+        const number: number = 3;
+
+        // Act
+        const value: string = fizzBuzz.evaluate(number);
+
+        // Assert
+        expect(value).toBe("Fizz")
+    });
+
+    it("Should return Buzz when number is multiple of five", () => {
+        // Arrange
+        const number: number = 5;
+
+        // Act
+        const value: string = fizzBuzz.evaluate(number);
+
+        // Assert
+        expect(value).toBe("Buzz")
+    });
 });
