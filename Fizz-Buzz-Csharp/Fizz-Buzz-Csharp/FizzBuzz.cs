9c8632cda7a0971c35d7b8957a5cc86d26784cec
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-29 00:08:02 +0100

    feat: add the first case for the kata in c-sharp should return the number ToString when it is not a multiplier of 3 or 5

---
+++ Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzz.cs
@@ -0,0 +1,9 @@
+namespace Fizz_Buzz_Csharp;
+
+public class FizzBuzz
+{
+    public String Evaluate(int number)
+    {
+        return number.ToString();
+    }
+}
---
+++ Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzzTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Fizz_Buzz_Csharp;
+
+internal class FizzBuzzTests
+{
+    [Test]
+    public void Should_Return_Same_Number_When_It_Is_Not_Multiple_Of_3_Or_5()
+    {
+        // Arrange
+        var number = 1;
+        var fizzBuzz = new FizzBuzz();
+
+        // Act
+        var value = fizzBuzz.Evaluate(number);
+
+        // Assert
+        Assert.That(value, Is.EqualTo("1"));
+    }
+}
