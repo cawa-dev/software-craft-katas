bf4b63f4bbb67a0fc386f9196a661159178ac071
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-29 00:32:35 +0100

    feat: refactor the final case for the multiple of three and five should return FizzBuzz using pattern matching and introducing new private method with constant

--- Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzz.cs
+++ Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzz.cs
@@ -2,14 +2,22 @@ namespace Fizz_Buzz_Csharp;
 
 internal class FizzBuzz
 {
+    private const int FizzMultiplier = 3;
+    private const int BuzzMultiplier = 5;
+    private const string Fizz = "Fizz";
+    private const string Buzz = "Buzz";
+
     public string Evaluate(int number)
     {
         return number switch
         {
-            _ when number % 15 == 0 => "FizzBuzz",
-            _ when number % 5 == 0 => "Buzz",
-            _ when number % 3 == 0 => "Fizz",
+            _ when IsMultipleOf(number, FizzMultiplier)
+                   && IsMultipleOf(number, BuzzMultiplier) => Fizz + Buzz,
+            _ when IsMultipleOf(number, BuzzMultiplier) => Buzz,
+            _ when IsMultipleOf(number, FizzMultiplier) => Fizz,
             _ => number.ToString()
         };
     }
+
+    private static bool IsMultipleOf(int number, int divider) => number % divider == 0;
 }
--- Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzzTests.cs
+++ Fizz-Buzz-Csharp/Fizz-Buzz-Csharp/FizzBuzzTests.cs
@@ -2,57 +2,61 @@
 
 internal class FizzBuzzTests
 {
+    private FizzBuzz _fizzBuzz;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fizzBuzz = new FizzBuzz();
+    }
+
     [Test]
     public void Should_Return_Same_Number_When_It_Is_Not_Multiple_Of_Three_Or_Five()
     {
         // Arrange
         const int number = 1;
-        var fizzBuzz = new FizzBuzz();
 
         // Act
-        var value = fizzBuzz.Evaluate(number);
+        var value = _fizzBuzz.Evaluate(number);
 
         // Assert
         Assert.That(value, Is.EqualTo("1"));
     }
-    
+
     [Test]
     public void Should_Return_Fizz_When_It_Is_Multiple_Of_Three()
     {
         // Arrange
         const int number = 3;
-        var fizzBuzz = new FizzBuzz();
 
         // Act
-        var value = fizzBuzz.Evaluate(number);
+        var value = _fizzBuzz.Evaluate(number);
 
         // Assert
         Assert.That(value, Is.EqualTo("Fizz"));
     }
-    
+
     [Test]
     public void Should_Return_Buzz_When_It_Is_Multiple_Of_Five()
     {
         // Arrange
         const int number = 5;
-        var fizzBuzz = new FizzBuzz();
 
         // Act
-        var value = fizzBuzz.Evaluate(number);
+        var value = _fizzBuzz.Evaluate(number);
 
         // Assert
         Assert.That(value, Is.EqualTo("Buzz"));
     }
-    
+
     [Test]
     public void Should_Return_FizzBuzz_When_It_Is_Multiple_Of_Three_And_Five()
     {
         // Arrange
         const int number = 15;
-        var fizzBuzz = new FizzBuzz();
 
         // Act
-        var value = fizzBuzz.Evaluate(number);
+        var value = _fizzBuzz.Evaluate(number);
 
         // Assert
         Assert.That(value, Is.EqualTo("FizzBuzz"));
