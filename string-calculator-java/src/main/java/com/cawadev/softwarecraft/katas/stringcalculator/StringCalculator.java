f110209314e20b11d14e314df89e16f5a4e8f124
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-30 21:14:25 +0100

    feat: add the first case should return 0 when the string is empty

---
+++ string-calculator-java/src/main/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.cawadev.softwarecraft.katas.stringcalculator;
+
+class StringCalculator {
+
+    public int add(String numbers) {
+        return 0;
+    }
+}
--- string-calculator-java/src/test/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculatorTest.java
+++ string-calculator-java/src/test/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculatorTest.java
@@ -1,4 +1,28 @@
 package com.cawadev.softwarecraft.katas.stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void should_return_zero_when_the_string_is_empty() {
+        // Arrange
+        String emptyString = "";
+
+        // Act
+        int result = stringCalculator.add(emptyString);
+
+        // Assert
+        assertThat(result).isEqualTo(0);
+    }
 }
