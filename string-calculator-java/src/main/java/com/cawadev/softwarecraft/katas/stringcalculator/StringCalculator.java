d22c7fe8d160cc8140360af9bffcaff75fac7798
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2025-01-30 21:30:57 +0100

    feat: add an edge case for the second case should return the sum of the numbers in the string separate by a comma with spaces inside

--- string-calculator-java/src/main/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculator.java
+++ string-calculator-java/src/main/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculator.java
@@ -1,8 +1,18 @@
 package com.cawadev.softwarecraft.katas.stringcalculator;
 
+import java.util.Arrays;
+
 class StringCalculator {
 
     public int add(String numbers) {
-        return numbers.isEmpty() ? 0 : Integer.parseInt(numbers);
+        if (numbers.isEmpty()) return 0;
+
+        String stringWithoutSpaces = numbers.replaceAll(" ", "");
+
+        String[] numbersArray = stringWithoutSpaces.split(",");
+
+        return Arrays.stream(numbersArray)
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
--- string-calculator-java/src/test/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculatorTest.java
+++ string-calculator-java/src/test/java/com/cawadev/softwarecraft/katas/stringcalculator/StringCalculatorTest.java
@@ -29,13 +29,37 @@ class StringCalculatorTest {
     @Test
     void should_return_the_same_number_when_one_number_is_passed() {
         // Arrange
-        String number = "1";
+        String number = "5";
 
         // Act
         int result = stringCalculator.add(number);
 
 
         // Assert
-        assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo(5);
+    }
+
+    @Test
+    void should_return_the_sum_of_the_number_separate_by_comma_in_the_string() {
+        // Arrange
+        String numbers = "5,4";
+
+        // Act
+        int result = stringCalculator.add(numbers);
+
+        // Assert
+        assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    void should_return_the_sum_of_numbers_separate_by_comma_in_the_string_with_space_inside() {
+        // Arrange
+        String numbers = "1, 2";
+
+        // Act
+        int result = stringCalculator.add(numbers);
+
+        // Assert
+        assertThat(result).isEqualTo(3);
     }
 }
