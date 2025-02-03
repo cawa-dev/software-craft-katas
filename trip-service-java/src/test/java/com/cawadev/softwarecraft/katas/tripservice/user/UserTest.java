78a0d1958c89d4ca2588a9e4e569b0782690d62f
Sacha MOUCHON <sacha.mouchon.carbon-it@ext.pmu.fr>
2025-02-03 22:55:43 +0100

    feat: add method isFriendsWith(User anotherUser) in User.class

--- trip-service-java/src/test/java/com/cawadev/softwarecraft/katas/tripservice/user/UserTest.java
+++ trip-service-java/src/test/java/com/cawadev/softwarecraft/katas/tripservice/user/UserTest.java
@@ -1,4 +1,20 @@
 package com.cawadev.softwarecraft.katas.tripservice.user;
 
-public class UserTest {
+import org.junit.jupiter.api.Test;
+
+import static com.cawadev.softwarecraft.katas.tripservice.user.UserBuilder.builder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    private static final User PAUL = new User();
+    private static final User JACQUES = new User();
+
+    @Test
+    void should_return_true_when_user_is_friends_with_another_user() {
+        var user = builder().friendsWith(PAUL).build();
+
+        assertThat(user.isFriendsWith(PAUL)).isTrue();
+        assertThat(user.isFriendsWith(JACQUES)).isFalse();
+    }
 }
