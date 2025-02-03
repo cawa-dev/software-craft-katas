8d0475f9fc53eb483a5641e713b465a5efd6bcad
Sacha MOUCHON <sacha.mouchon.carbon-it@ext.pmu.fr>
2025-02-03 23:01:06 +0100

    refactor: refactor the code of the TripService

--- trip-service-java/src/main/java/com/cawadev/softwarecraft/katas/tripservice/trip/TripService.java
+++ trip-service-java/src/main/java/com/cawadev/softwarecraft/katas/tripservice/trip/TripService.java
@@ -10,23 +10,13 @@ import java.util.List;
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
-        User loggedUser = getLoggedUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = tripsByUser(user);
-            }
-            return tripList;
-        } else {
+        if (getLoggedUser() == null) {
             throw new UserNotLoggedInException();
         }
+
+        return user.isFriendsWith(getLoggedUser())
+                ? tripsByUser(user) :
+                new ArrayList<>();
     }
 
     protected List<Trip> tripsByUser(User user) {
